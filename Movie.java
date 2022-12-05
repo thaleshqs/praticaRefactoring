e65facfecc4dd620509c1805560c59b5a729ef39
thaleshqs <59453636+thaleshqs@users.noreply.github.com>
2022-12-05 12:26:06 -0300

    Commit 14
    Commit 14

--- Movie.java
+++ Movie.java
@@ -40,10 +40,6 @@ public class Movie {
     return _price.getCharge(daysRented);
   }
   public int getFrequentRenterPoints(int daysRented) {
-      if ((getPriceCode() == Movie.NEW_RELEASE) &&
-         daysRented > 1) 
-          return 2;
-      else
-          return 1;
+    return _price.getFrequentRenterPoints(daysRented);
   }
 }
